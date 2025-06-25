c3bb6b91e66d8e28546b242ba5bb519af08ca6fe
mioe <mioe.boss@gmail.com>
2025-06-25 22:10:24 +0300

    feat: fix build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+/// <reference types="@figma/widget-typings" />
+
+// Добавляем глобальные типы Figma
+declare global {
+	const figma: {
+		widget: WidgetAPI
+	}
+	const __html__: string
+}
+
+export {}
